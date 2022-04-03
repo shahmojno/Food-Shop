5421af2edb07200b2d9e2c0b0ea2e9b0c86dfc09
shahmojno <mojno12@gmail.com>
2022-04-03 17:04:56 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>This food shop:</h1>
+      <LoadData></LoadData>
     </div>
   );
 }
 
+function LoadData() {
+  const [foods, setFoods] = useState([]);
+  useEffect(() => {
+    fetch('data.json')
+      .then(res => res.json())
+      .then(data => setFoods(data))
+  }, [])
+  return (
+
+    <div>
+
+      <h1>Names: {foods.length}</h1>
+      {
+        foods.map(food => <Foods id={food.id} name={food.name} img={food.img}></Foods>)
+      }
+
+    </div >
+  )
+
+}
+
+function Foods(props) {
+  return (
+    <div>
+      <h4> Id : {props.id}</h4>
+      <p>Name : {props.name}</p>
+      <p>img : {props.img}</p>
+      <div className='photo'>
+        <img src={props.img} alt="" />
+      </div>
+
+    </div>
+  )
+}
+
 export default App;
+
